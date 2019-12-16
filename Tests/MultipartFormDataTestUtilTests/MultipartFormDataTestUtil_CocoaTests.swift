5bf8b0e5e035f56b015196756e7aa4d3f478c7c0
417-72KI <417.72ki@gmail.com>
2019-12-16 16:02:29 +0900

    Divide by platform

---
+++ Tests/MultipartFormDataTestUtilTests/MultipartFormDataTestUtil_CocoaTests.swift
@@ -0,0 +1,67 @@
+import XCTest
+import Alamofire
+import OHHTTPStubs
+import OHHTTPStubsSwift
+
+import MultipartFormDataTestUtil
+
+#if canImport(Cocoa)
+import Cocoa
+final class MultipartFormDataTestUtil_CocoaTests: XCTestCase {
+
+    override class func setUp() {
+        super.setUp()
+        let condition = isHost("localhost")
+            && isPath("/upload")
+        stub(condition: condition, response: uploadTestStubResponse)
+    }
+
+    override class func tearDown() {
+        HTTPStubs.removeAllStubs()
+        super.tearDown()
+    }
+
+    func testAlamofire() throws {
+        let genbaNeko = try XCTUnwrap(NSImage(data: TestResource.genbaNeko)?.jpegRepresentation)
+        let denwaNeko = try XCTUnwrap(NSImage(data: TestResource.denwaNeko)?.jpegRepresentation)
+        let message = try XCTUnwrap("Hello world!".data(using: .utf8))
+
+        let exp = expectation(description: "response")
+
+        AF.upload(
+            multipartFormData: { formData in
+                formData.append(
+                    genbaNeko,
+                    withName: "genbaNeko",
+                    fileName: "genbaNeko.jpeg",
+                    mimeType: "image/jpeg"
+                )
+                formData.append(
+                    denwaNeko,
+                    withName: "denwaNeko",
+                    fileName: "denwaNeko.jpeg",
+                    mimeType: "image/jpeg"
+                )
+                formData.append(message, withName: "message")
+            },
+            to: "https://localhost/upload"
+            ).responseJSON {
+                switch $0.result {
+                case let .success(data):
+                    do {
+                        let dic = try XCTUnwrap(data as? [String: Any])
+                        let status = try XCTUnwrap(dic["status"] as? Int)
+                        XCTAssertEqual(status, 200)
+                        XCTAssertNil(dic["error"])
+                    } catch {
+                        XCTFail(error.localizedDescription)
+                    }
+                case let .failure(error):
+                    XCTFail(error.localizedDescription)
+                }
+                exp.fulfill()
+        }
+        wait(for: [exp], timeout: 10)
+    }
+}
+#endif
---
+++ Tests/MultipartFormDataTestUtilTests/TestStub.swift
@@ -0,0 +1,38 @@
+#if canImport(Cocoa)
+import Cocoa
+typealias Image = NSImage
+#endif
+
+#if canImport(UIKit)
+import UIKit
+typealias Image = UIImage
+#endif
+
+import OHHTTPStubs
+import OHHTTPStubsSwift
+
+import MultipartFormDataTestUtil
+
+var uploadTestStubResponse: OHHTTPStubsResponseBlock = { request in
+    let errorResponse = { (message: String) -> OHHTTPStubsResponse in
+        .init(jsonObject: ["status": 403, "error": message], statusCode: 403, headers: ["Content-Type": "application/json"])
+    }
+    do {
+        let data = try MultipartFormData.parse(from: request)
+        guard let genbaNeko = data.element(forName: "genbaNeko") else { return errorResponse("genbaNeko") }
+        guard let denwaNeko = data.element(forName: "denwaNeko") else { return errorResponse("denwaNeko") }
+        guard let message = data.element(forName: "message") else { return errorResponse("message") }
+        guard let _ = Image(data: genbaNeko.data) else { return errorResponse("Unexpected genbaNeko") }
+        // try genbaNeko.data.write(to: URL(fileURLWithPath: ("~/work/genbaNeko.jpg" as NSString).expandingTildeInPath), options: .atomicWrite)
+        guard let _ = Image(data: denwaNeko.data) else { return errorResponse("Unexpected denwaNeko") }
+        // try denwaNeko.data.write(to: URL(fileURLWithPath: ("~/work/denwaNeko.jpg" as NSString).expandingTildeInPath), options: .atomicWrite)
+        guard message.string == "Hello world!" else { return errorResponse("Unexpected message: \(message)") }
+    } catch {
+        return .init(error: error)
+    }
+    return .init(
+        jsonObject: ["status": 200],
+        statusCode: 200,
+        headers: ["Content-Type": "application/json"]
+    )
+}
