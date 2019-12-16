02154a99f893f8b10d44a63b7276251ca1106a56
417-72KI <417.72ki@gmail.com>
2019-12-16 17:06:45 +0900

    Pending watchOS support

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         .macOS(.v10_14),
         .iOS(.v11),
         .tvOS(.v11),
-        .watchOS(.v4)
+//        .watchOS(.v4)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
