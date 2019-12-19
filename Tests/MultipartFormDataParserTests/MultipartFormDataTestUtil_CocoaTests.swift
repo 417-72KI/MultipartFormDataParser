db9c03e0092fe1833573dcacec57521e1710da4b
417-72KI <417.72ki@gmail.com>
2019-12-19 21:44:54 +0900

    Test case I/F

--- Tests/MultipartFormDataParserTests/MultipartFormDataTestUtil_CocoaTests.swift
+++ Tests/MultipartFormDataParserTests/MultipartFormDataTestUtil_CocoaTests.swift
@@ -33,5 +33,15 @@ final class MultipartFormDataParser_CocoaTests: XCTestCase {
         XCTAssertEqual(result.status, 200)
         XCTAssertNil(result.error)
     }
+
+    func testMoya() throws {
+        let genbaNeko = try XCTUnwrap(NSImage(data: TestResource.genbaNeko)?.jpegRepresentation)
+        let denwaNeko = try XCTUnwrap(NSImage(data: TestResource.denwaNeko)?.jpegRepresentation)
+        let message = try XCTUnwrap("Hello world!".data(using: .utf8))
+
+        let result = try XCTUnwrap(uploadWithMoya(genbaNeko: genbaNeko, denwaNeko: denwaNeko, message: message))
+        XCTAssertEqual(result.status, 200)
+        XCTAssertNil(result.error)
+    }
 }
 #endif
---
+++ Tests/MultipartFormDataParserTests/TestFunctions/TestFunction_Moya.swift
@@ -0,0 +1,15 @@
+import Foundation
+import Moya
+import XCTest
+
+extension XCTestCase {
+    func uploadWithMoya(
+        genbaNeko: Data,
+        denwaNeko: Data,
+        message: Data,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) throws -> TestEntity? {
+        return nil
+    }
+}
