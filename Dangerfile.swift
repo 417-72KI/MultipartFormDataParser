192b4c968d4b5aafe173028813f6d1036c968095
417-72KI <417.72ki@gmail.com>
2020-08-28 11:44:44 +0900

    create Dangerfile.swift

---
+++ Dangerfile.swift
@@ -0,0 +1,24 @@
+import Danger 
+let danger = Danger()
+
+SwiftLint.lint(inline: true)
+
+let git = danger.git
+
+if git.modifiedFiles.contains("MultipartFormDataParser.xcodeproj") {
+    danger.fail("Do not modify xcodeproj. This file is modified only on release.")
+}
+
+if git.modifiedFiles.contains("LICENSE") {
+    danger.fail("Do not modify LICENSE !!")
+}
+
+if git.deletedFiles.contains("LICENSE") {
+    danger.fail("Do not delete LICENSE !!")
+}
+
+if let github = danger.github {
+    if github.pullRequest.title.lowercased().contains("[wip]") {
+        danger.warn("PR is classed as Work in Progress")
+    }
+}
