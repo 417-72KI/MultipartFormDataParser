eb3fd2ead7d20d6569c26620bc051af211cce3a8
417-72KI <417.72ki@gmail.com>
2020-08-31 15:08:48 +0900

    don't create xcodeproj

--- scripts/release.sh
+++ scripts/release.sh
@@ -15,13 +15,6 @@ if [ "${CURRENT_BRANCH}" = '' ]; then
     exit 1
 fi
 
-# Generate xcodeproj
-swift package generate-xcodeproj
-
-git add -f ${PACKAGE_NAME}.xcodeproj
-git commit -m 'Update xcodeproj'
-git push origin
-
 # TAG
 TAG=$(cat Sources/MultipartFormDataParser/Info.swift | grep version | awk '{ print $NF }' | sed -E 's/\"(.*)\"/\1/')
 git tag ${TAG}
