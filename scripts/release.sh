2077fe62c6d70fc7f3650d02208f1537ebedf801
417-72KI <417.72ki@gmail.com>
2020-08-31 15:11:44 +0900

    fix release.sh

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,22 +1,41 @@
 #!/bin/zsh
 
+set -eu
+
+APPLICATION_INFO_FILE='Sources/MultipartFormDataParser/Info.swift'
+
 if ! type "github-release" > /dev/null; then
     echo '`github-release` not found. Install'
-    go get github.com/aktau/github-release
+    go get github.com/github-release/github-release
 fi
 
 cd $(git rev-parse --show-toplevel)
 
 PACKAGE_NAME='MultipartFormDataParser'
 
-CURRENT_BRANCH=$(git branch | grep '* master')
-if [ "${CURRENT_BRANCH}" = '' ]; then
-    echo '[Error] this script must be run in master branch.'
+if [ `git symbolic-ref --short HEAD` != 'master' ]; then
+    echo '\e[31mRelease is enabled only in master.\e[m'
+    exit 1
+fi
+
+if [ "$(git status -s | grep "${APPLICATION_INFO_FILE}")" = '' ]; then
+    echo "\e[31m${APPLICATION_INFO_FILE} is not modified.\e[m"
+    exit 1
+fi
+
+if [ "$(git status -s | grep .swift | grep -v ${APPLICATION_INFO_FILE})" != '' ]; then
+    echo "\e[31mUnexpected added/modified/deleted file.\e[m"
     exit 1
 fi
 
 # TAG
-TAG=$(cat Sources/MultipartFormDataParser/Info.swift | grep version | awk '{ print $NF }' | sed -E 's/\"(.*)\"/\1/')
+TAG=$(cat "${APPLICATION_INFO_FILE}" | grep version | awk '{ print $NF }' | sed -E 's/\"(.*)\"/\1/')
+if [ "$(git tag | grep ${TAG})" != '' ]; then
+    echo "\e[31mTag: '${TAG}' already exists.\e[m"
+    exit 1
+fi
+
+git commit -m "Bump version to ${TAG}" "${APPLICATION_INFO_FILE}"
 git tag ${TAG}
 git push origin ${TAG}
 
