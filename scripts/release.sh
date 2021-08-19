e393fb428926bdafe57f314eee35daf713850878
417-72KI <417.72ki@gmail.com>
2021-08-20 03:09:26 +0900

    fix release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -15,15 +15,33 @@ if [ "${CURRENT_BRANCH}" = '' ]; then
     exit 1
 fi
 
+if [ "`git diff --name-only`" != '' ]; then
+    echo '[Error] There are some local changes.'
+    exit 1
+fi
+
+TAG=$(cat Sources/MultipartFormDataParser/Info.swift | grep version | awk '{ print $NF }' | sed -E 's/\"(.*)\"/\1/')
+
+# Validate
+README_VERSION=$(cat README.md | grep '.package(url: ' | awk '{ print $NF }' | sed -E 's/\"(.*)\"\)?/\1/')
+if [ "${TAG}" != "${README_VERSION}" ]; then
+    echo '[Error] README.md not updated. Match version in installation.'
+    exit 1
+fi
+
+if [ "$(git fetch --tags && git tag | grep "${TAG}")" != '' ]; then
+    echo "[Error] '${TAG}' tag already exists."
+    exit 1
+fi
+
 # Generate xcodeproj
 swift package generate-xcodeproj
 
-git add -f ${PACKAGE_NAME}.xcodeproj
+git add ${PACKAGE_NAME}.xcodeproj
 git commit -m 'Update xcodeproj'
 git push origin
 
 # TAG
-TAG=$(cat Sources/MultipartFormDataParser/Info.swift | grep version | awk '{ print $NF }' | sed -E 's/\"(.*)\"/\1/')
 git tag ${TAG}
 git push origin ${TAG}
 
