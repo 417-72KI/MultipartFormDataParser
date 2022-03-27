c55ccc43de26269ec9faefaea955022575ea1733
417-72KI <417.72ki@gmail.com>
2022-03-27 17:35:46 +0900

    Bump test dependencies

--- Package.swift
+++ Package.swift
@@ -8,9 +8,9 @@ let isRelease = false
 let testDependencies: [Package.Dependency] = isRelease
 ? []
 : [
-    .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.4.4"),
+    .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.5.0"),
     .package(url: "https://github.com/AliSoftware/OHHTTPStubs.git", from: "9.1.0"),
-    .package(url: "https://github.com/ishkawa/APIKit.git", from: "5.2.0"),
+    .package(url: "https://github.com/ishkawa/APIKit.git", from: "5.3.0"),
     .package(url: "https://github.com/Moya/Moya.git", from: "15.0.0"),
 ]
 let testTargetDependencies: [Target.Dependency] = isRelease
