f064673611d9404aeb73f1ecb6ce074479b9c00b
417-72KI <417.72ki@gmail.com>
2022-04-16 02:45:13 +0900

    add stub response

--- Tests/MultipartFormDataParserTests/TestStub.swift
+++ Tests/MultipartFormDataParserTests/TestStub.swift
@@ -1,3 +1,8 @@
+import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
+
 #if canImport(Cocoa) && !targetEnvironment(macCatalyst)
 import Cocoa
 typealias Image = NSImage
@@ -20,17 +25,22 @@ func stubForUpload() {
     let condition = isHost("localhost")
         && isPath("/upload")
     stub(condition: condition, response: uploadTestStubResponse)
+    #else
+    StubURLProtocol.requestHandler = uploadTestStubResponse
+    URLProtocol.registerClass(StubURLProtocol.self)
     #endif
 }
 
 func clearStubs() {
     #if canImport(OHHTTPStubs)
     HTTPStubs.removeAllStubs()
+    #else
+    URLProtocol.unregisterClass(StubURLProtocol.self)
     #endif
 }
 
 #if canImport(OHHTTPStubs)
-private var uploadTestStubResponse: HTTPStubsResponseBlock = { request in
+private let uploadTestStubResponse: HTTPStubsResponseBlock = { request in
     let errorResponse = { (message: String) -> HTTPStubsResponse in
         .init(jsonObject: ["status": 403, "error": message], statusCode: 403, headers: ["Content-Type": "application/json"])
     }
@@ -50,5 +60,35 @@ private var uploadTestStubResponse: HTTPStubsResponseBlock = { request in
         statusCode: 200,
         headers: ["Content-Type": "application/json"]
     )
+}
+#else
+private let uploadTestStubResponse: StubURLProtocol.RequestHandler = { request in
+    let errorResponse = { (message: String) -> (Data?, HTTPURLResponse) in
+        (
+            #"{"status": 403, "error": "\#(message)"}"#.data(using: .utf8),
+            HTTPURLResponse(url: request.url!,
+                            statusCode: 403,
+                            httpVersion: "HTTP/2",
+                            headerFields: ["Content-Type": "application/json"])!
+        )
+    }
+    do {
+        let data = try MultipartFormData.parse(from: request)
+        guard let genbaNeko = data.element(forName: "genbaNeko") else { return errorResponse("genbaNeko") }
+        guard let denwaNeko = data.element(forName: "denwaNeko") else { return errorResponse("denwaNeko") }
+        guard let message = data.element(forName: "message") else { return errorResponse("message") }
+        guard let _ = Image(data: genbaNeko.data) else { return errorResponse("Unexpected genbaNeko") }
+        guard let _ = Image(data: denwaNeko.data) else { return errorResponse("Unexpected denwaNeko") }
+        guard message.string == "Hello world!" else { return errorResponse("Unexpected message: \(message)") }
+        return (
+            #"{"status": 200}"#.data(using: .utf8),
+            HTTPURLResponse(url: request.url!,
+                            statusCode: 200,
+                            httpVersion: "HTTP/2",
+                            headerFields: ["Content-Type": "application/json"])!
+        )
+    } catch {
+        return errorResponse(error.localizedDescription)
+    }
 }
 #endif
