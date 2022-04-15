6617a163ca652bb73884337ed45fe78ab351dfae
417-72KI <417.72ki@gmail.com>
2022-04-16 02:44:49 +0900

    create stub for `URLProtocol`

---
+++ Tests/MultipartFormDataParserTests/StubURLProtocol.swift
@@ -0,0 +1,38 @@
+import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
+
+final class StubURLProtocol: URLProtocol {
+    typealias RequestHandler = (URLRequest) throws -> (Data?, HTTPURLResponse)
+
+    static var requestHandler: RequestHandler?
+
+    override class func canInit(with request: URLRequest) -> Bool {
+        true
+    }
+
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        request
+    }
+
+    override func startLoading() {
+        guard let requestHandler = Self.requestHandler else { return }
+        do {
+            let (data, response) = try requestHandler(request)
+            client?.urlProtocol(self,
+                                didReceive: response,
+                                cacheStoragePolicy: .notAllowed)
+            if let data = data {
+                client?.urlProtocol(self, didLoad: data)
+            }
+            client?.urlProtocolDidFinishLoading(self)
+        } catch {
+            client?.urlProtocol(self, didFailWithError: error)
+        }
+    }
+
+    override func stopLoading() {
+        // no-op
+    }
+}
